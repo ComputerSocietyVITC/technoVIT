3b2c1c3dd2f1e5ad234f61a394187890508f4900
varun <varunrohit2000@gmail.com>
2019-09-13 19:44:00 +0530

    cganing colours of blocks

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -9,9 +9,17 @@ public class Spawner : MonoBehaviour
     public float spawngap = 5;
     public float x = 7;
     float nxtst;
+    public Material white;
+    public Material blue;
+    public Material yellow;
+    public MeshRenderer rendcube;
+    public MeshRenderer rendcaps;
+    static int i;
     // Start is called before the first frame update
     void Start()
-    { }
+    {
+        i=1;
+     }
 
     // Update is called once per frame
     void Update()
@@ -22,6 +30,26 @@ public class Spawner : MonoBehaviour
             no = Random.Range(1,100);
             nxtst = Time.time + spawngap;
             Vector3 spawnpos = new Vector3(Random.Range(-x, x), 6, 0);
+            i = Random.Range(1, 3);
+            if(i==1)
+            {
+                rendcube.material = yellow;
+                rendcaps.material = yellow;
+                // i++;
+            }
+            else if(i==2)
+            {
+                rendcube.material = blue;
+                rendcaps.material = blue;
+                // i++;
+            }
+            else if(i==3)
+            {
+                rendcube.material = white;
+                rendcaps.material = white;
+                // i=1;
+            }
+
             if (no%2==0)
             {
                 Instantiate(ob1, spawnpos, Quaternion.identity);
